6882819d98cfa4b0b6c56e05927c5ab6e8fdeb75
Ikenna Okpala <me@ikennaokpala.com>
2016-05-03 22:55:15 +0100

    Cosmetic change

--- templates/entrypoint.sh
+++ templates/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 export PATH=/usr/local/bin:/usr/lib/postgresql/$POSTGRES_VERSION/bin/:$PATH
-export NUT_PG_DSN="postgres://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_IP/$POSTGRES_DB?sslmode=disable"
+export NUT_PG_DSN="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_IP/$POSTGRES_DB?sslmode=disable"
 
 /etc/init.d/pg_entrypoint.sh $POSTGRES_MAIN_USER
