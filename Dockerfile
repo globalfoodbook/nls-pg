efb941efe51c66461ff684ebf8705a7c469061d3
Ikenna Okpala <me@ikennaokpala.com>
2016-05-04 00:58:03 +0100

    Comment out to test and resolve issues
    Switch back to plain sql format
    
    Direct load
    
    Add role
    
    Update

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ ENV POSTGRES_VERSION 9.5
 ENV POSTGRES_IP 127.0.0.1
 ENV POSTGRES_PORT 5432
 ENV POSTGRES_MAIN_USER postgres
-ENV POSTGRES_USER $POSTGRES_MAIN_USER
+ENV POSTGRES_USER ikennaokpala
 ENV POSTGRES_PASSWORD 12345678
-ENV POSTGRES_DB $POSTGRES_USER
-ENV NUT_PG_DSN `postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_IP/$POSTGRES_DB?sslmode=disable`
+ENV POSTGRES_DB nutrition_development
+ENV NUT_PG_DSN postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_IP/$POSTGRES_DB?sslmode=disable
 ENV PATH /usr/local/bin:/usr/lib/postgresql/$POSTGRES_VERSION/bin/:$PATH
 
 RUN mkdir -p /tmp/psql_data/
@@ -17,7 +17,8 @@ RUN mkdir -p /tmp/psql_data/
 ADD templates/entrypoint.sh /etc/init.d/entrypoint.sh
 ADD templates/pg_entrypoint.sh /etc/init.d/pg_entrypoint.sh
 
-COPY templates/nutrition_sr26_plain.sql /tmp/psql_data/
+COPY templates/nutrition_sr26.sql /tmp/psql_data/
+COPY templates/nutrition_sr26.archive /tmp/psql_data/
 COPY templates/load_data.sh /docker-entrypoint-initdb.d/
 
 RUN chmod +x /docker-entrypoint-initdb.d/load_data.sh
--- templates/load_data.sh
+++ templates/load_data.sh
@@ -2,36 +2,46 @@
 
 set -e
 
-export DB_DUMP_LOCATION=/tmp/psql_data/nutrition_sr26_plain.sql
+export DB_DUMP_PLAIN_LOCATION=/tmp/psql_data/nutrition_sr26.sql
+# export DB_DUMP_ARCHIVE_LOCATION=/tmp/psql_data/nutrition_sr26.archive
 
 echo -e "*** LOADING DATABASE ***"
 
 # create default database
-# gosu postgres $POSTGRES_MAIN_USER --single <<EOSQL
+# gosu $POSTGRES_MAIN_USER $POSTGRES_MAIN_USER --single <<-EOSQL
 #   CREATE DATABASE "$POSTGRES_DB";
-#   GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO postgres;
-# EOSQ
+#   GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO $POSTGRES_MAIN_USER;
+# EOSQL
 
 # clean sql_dump - because I want to have a one-line command
 
 # remove indentation
-sed 's/^[ \t]*//' -i $DB_DUMP_LOCATION
+# sed 's/^[ \t]*//' -i $DB_DUMP_PLAIN_LOCATION
 
 # remove comments
-sed '/^--/ d' -i $DB_DUMP_LOCATION
+# sed '/^--/ d' -i $DB_DUMP_PLAIN_LOCATION
 
 # remove new lines
-sed ':a;N;$!ba;s/\n/ /g' -i $DB_DUMP_LOCATION
+# sed ':a;N;$!ba;s/\n/ /g' -i $DB_DUMP_PLAIN_LOCATION
 
 # remove other spaces
-sed 's/  */ /g' -i $DB_DUMP_LOCATION
+# sed 's/  */ /g' -i $DB_DUMP_PLAIN_LOCATION
 
 # remove firsts line spaces
-sed 's/^ *//' -i $DB_DUMP_LOCATION
+# sed 's/^ *//' -i $DB_DUMP_PLAIN_LOCATION
 
 # append new line at the end
-sed -e '$a\' -i $DB_DUMP_LOCATION
+# sed -e '$a\' -i $DB_DUMP_PLAIN_LOCATION
 
-gosu postgres psql -h $POSTGRES_IP -p $POSTGRES_PORT -U $POSTGRES_DB -d $POSTGRES_DB -a -f $DB_DUMP_LOCATION
+# sed -i s"/ikennaokpala/$POSTGRES_USER/" $DB_DUMP_PLAIN_LOCATION
+
+# gosu $POSTGRES_MAIN_USER psql -h $POSTGRES_IP -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -a -f $DB_DUMP_PLAIN_LOCATION
+# gosu $POSTGRES_MAIN_USER pg_restore --verbose --clean --no-acl --no-owner -h $POSTGRES_IP -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB $DB_DUMP_ARCHIVE_LOCATION
+
+# gosu $POSTGRES_MAIN_USER psql -h $POSTGRES_IP -p $POSTGRES_PORT -U $POSTGRES_USER  <<-EOSQL
+#   CREATE ROLE "ikennaokpala";
+# EOSQL
+
+gosu $POSTGRES_MAIN_USER psql -h $POSTGRES_IP -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB < $DB_DUMP_PLAIN_LOCATION
 
 echo -e "*** DATABASE LOADED! ***"
